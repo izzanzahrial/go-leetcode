07992ab3cf0640ecbba477d00a9f7ba70d61f842
Ahmad Izzan Zahrial <izzanzahrial@outlook.com>
2022-07-07 22:28:23 +0700

    fix min cost all points

--- advanceGraph/minCostAllPoints.go
+++ advanceGraph/minCostAllPoints.go
@@ -6,33 +6,33 @@ import (
 )
 
 // https://leetcode.com/problems/min-cost-to-connect-all-points/
-// still wrong
+// still wrong the manhattan algorithm
 func minCostConnectPoints(points [][]int) int {
 	var result int
-	hashMin := make(map[[2]int]int)
+	pointsMap := make(map[[2]int]int)
 
-	for i := 0; i < len(points); i++ {
-		currPoint := [2]int{points[i][0], points[i][1]}
-		hashMin[currPoint] = math.MaxInt64
+	for _, point := range points {
+		currPoint := [2]int{point[0], point[1]}
+		pointsMap[currPoint] = math.MaxInt64
 	}
 
 	for i := 0; i < len(points); i++ {
-		for j := i + 1; j < len(points); j++ {
+		for j := 0; j < len(points); j++ {
+			if i == j {
+				continue
+			}
 			currPoint := [2]int{points[i][0], points[i][1]}
 			nextPoint := [2]int{points[j][0], points[j][1]}
-			currVal := int(math.Abs(float64(currPoint[0]-nextPoint[0]) + float64(currPoint[1]-nextPoint[1])))
-			if currVal < hashMin[currPoint] {
-				hashMin[currPoint] = currVal
+			// this part, manhattan algorithm
+			currVal := int(math.Abs(float64(currPoint[0]-nextPoint[0])) + math.Abs(float64(currPoint[1]-nextPoint[0])))
+			if currVal < pointsMap[currPoint] {
+				pointsMap[currPoint] = currVal
 			}
 		}
 	}
 
-	fmt.Println(hashMin)
-
-	for _, val := range hashMin {
-		if val == 9223372036854775807 {
-			val = 0
-		}
+	for key, val := range pointsMap {
+		fmt.Println(key, val)
 		result += val
 	}
 
