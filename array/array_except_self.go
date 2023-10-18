105d72a2554e829090dc655b85af27aeac668566
izzan zahrial <izzanzahrial@outlook.com>
2023-10-18 10:29:25 +0700

    add increasing triplet

--- array/array_except_self.go
+++ array/array_except_self.go
@@ -1,8 +1,8 @@
-package main
+package array
 
 // https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75
 // https://www.youtube.com/watch?v=bNvIQI2wAjk&ab_channel=NeetCode
-func productExceptSelf(nums []int) []int {
+func ProductExceptSelf(nums []int) []int {
 	result := make([]int, len(nums))
 
 	// count for prefix
