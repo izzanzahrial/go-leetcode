61e86e0aef26d96019c85c7904e6614ad9aa060a
izzan zahrial <izzanzahrial@outlook.com>
2023-12-05 13:46:26 +0700

    add search insert position

---
+++ array/search_insert_position.go
@@ -0,0 +1,25 @@
+package array
+
+func SearchInsert(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := left + ((right - left) / 2)
+		val := nums[mid]
+
+		if val == target {
+			return mid
+		}
+
+		if val < target {
+			left = mid + 1
+		}
+
+		if val > target {
+			right = mid - 1
+		}
+	}
+
+	return left
+}
