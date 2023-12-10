d33338abe59af64853ebe196432bd1be6808b782
izzan zahrial <izzanzahrial@outlook.com>
2023-12-10 14:11:02 +0700

    add another way for merge two linked list

--- linkedList/merge_2_sort_list.go
+++ linkedList/merge_2_sort_list.go
@@ -8,23 +8,55 @@ package linkedlist
  * }
  */
 // https://leetcode.com/problems/merge-two-sorted-lists/description/
+// func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+// 	result := &ListNode{}
+
+// 	for curr := result; list1 != nil || list2 != nil; curr = curr.Next {
+// 		if list1 == nil {
+// 			curr.Next = list2
+// 			break
+// 		} else if list2 == nil {
+// 			curr.Next = list1
+// 			break
+// 		} else if list1.Val < list2.Val {
+// 			curr.Next = list1
+// 			list1 = list1.Next
+// 		} else {
+// 			curr.Next = list2
+// 			list2 = list2.Next
+// 		}
+// 	}
+
+// 	return result.Next
+// }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	result := &ListNode{}
+	curr := result
 
-	for curr := result; list1 != nil || list2 != nil; curr = curr.Next {
-		if list1 == nil {
-			curr.Next = list2
-			break
-		} else if list2 == nil {
-			curr.Next = list1
-			break
-		} else if list1.Val < list2.Val {
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
 			curr.Next = list1
 			list1 = list1.Next
 		} else {
 			curr.Next = list2
 			list2 = list2.Next
 		}
+
+		curr = curr.Next
+	}
+
+	if list1 != nil {
+		curr.Next = list1
+	} else if list2 != nil {
+		curr.Next = list2
 	}
 
 	return result.Next
