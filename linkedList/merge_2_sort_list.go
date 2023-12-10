9f523c5b4fbcefd93c72256cdeca9f8cfd9e21a8
izzan zahrial <izzanzahrial@outlook.com>
2023-12-10 13:58:25 +0700

    add merge two linked list

---
+++ linkedList/merge_2_sort_list.go
@@ -0,0 +1,31 @@
+package linkedlist
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	result := &ListNode{}
+
+	for curr := result; list1 != nil || list2 != nil; curr = curr.Next {
+		if list1 == nil {
+			curr.Next = list2
+			break
+		} else if list2 == nil {
+			curr.Next = list1
+			break
+		} else if list1.Val < list2.Val {
+			curr.Next = list1
+			list1 = list1.Next
+		} else {
+			curr.Next = list2
+			list2 = list2.Next
+		}
+	}
+
+	return result.Next
+}
