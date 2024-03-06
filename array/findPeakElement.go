ef59603739e7e5e434ff3561435f6e7511edcf59
izzan zahrial <izzanzahrial@outlook.com>
2024-03-06 11:39:19 +0700

    add find peak element

---
+++ array/findPeakElement.go
@@ -0,0 +1,40 @@
+package array
+
+// https://leetcode.com/problems/find-peak-element/description/
+func FindPeakElement(nums []int) int {
+	if len(nums) <= 1 {
+		return 0
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if isPeak(i, nums) {
+			return i
+		}
+	}
+
+	return 0
+}
+
+func isPeak(idx int, nums []int) bool {
+	if idx == 0 {
+		if nums[idx] > nums[idx+1] {
+			return true
+		}
+
+		return false
+	}
+
+	if idx == len(nums)-1 {
+		if nums[idx] > nums[idx-1] {
+			return true
+		}
+
+		return false
+	}
+
+	if nums[idx] > nums[idx+1] && nums[idx] > nums[idx-1] {
+		return true
+	}
+
+	return false
+}
