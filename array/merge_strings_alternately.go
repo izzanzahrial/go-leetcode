33df891e9ee84c0ea04fd661d78a574861070fd7
izzan zahrial <izzanzahrial@outlook.com>
2024-07-31 13:05:12 +0700

    [array] merge strings alternately using strings builder

--- array/merge_strings_alternately.go
+++ array/merge_strings_alternately.go
@@ -1,5 +1,10 @@
 package array
 
+import (
+	"log"
+	"strings"
+)
+
 // https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
 func MergeAlternately(word1 string, word2 string) string {
 	var result string
@@ -23,4 +28,43 @@ func MergeAlternately(word1 string, word2 string) string {
 	}
 
 	return result
+}
+
+func mergeAlternately(word1 string, word2 string) string {
+	var result strings.Builder
+
+	for len(word1) != 0 && len(word2) != 0 {
+		err := result.WriteByte(word1[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = result.WriteByte(word2[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		word1 = word1[1:]
+		word2 = word2[1:]
+	}
+
+	for len(word1) != 0 {
+		err := result.WriteByte(word1[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		word1 = word1[1:]
+	}
+
+	for len(word2) != 0 {
+		err := result.WriteByte(word2[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		word2 = word2[1:]
+	}
+
+	return result.String()
 }
