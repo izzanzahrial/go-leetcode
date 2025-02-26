45e680e8d09ec19ed6abdbcab7a2301d045ae41c
izzan zahrial <izzanzahrial@outlook.com>
2025-02-26 14:55:08 +0700

    combination sum

--- backtrack/combinationSum.go
+++ backtrack/combinationSum.go
@@ -2,29 +2,37 @@ package backtrack
 
 // https://leetcode.com/problems/combination-sum/
 // https://www.youtube.com/watch?v=GBKI9VSKdGg&ab_channel=NeetCode
-func CombinationSum(candidates []int, target int) [][]int {
+func combinationSum(candidates []int, target int) [][]int {
+	if target == 0 {
+		return nil
+	}
+
 	var result [][]int
 	var curr []int
-
-	backtrack1(0, 0, target, candidates, curr, &result)
-
+	backtrack(0, 0, target, curr, candidates, &result)
 	return result
 }
 
-func backtrack1(i, total, target int, candidates, curr []int, result *[][]int) {
-	if total == target {
-		valid := make([]int, len(curr))
-		copy(valid, curr)
-		*result = append(*result, valid)
+func backtrack1(idx, currTotal, target int, curr, candidates []int, result *[][]int) {
+	// check if the current total > target
+	// and check to make sure we don't go the last index + 1
+	if currTotal > target || idx >= len(candidates) {
 		return
 	}
 
-	if i >= len(candidates) || total > target {
+	if currTotal == target {
+		// to make sure the current result doesn't get updated by the previous call
+		// create copy of the current result
+		currCopy := make([]int, len(curr))
+		copy(currCopy, curr)
+		*result = append(*result, currCopy)
 		return
 	}
 
-	curr = append(curr, candidates[i])
-	backtrack1(i, total+candidates[i], target, candidates, curr, result)
+	curr = append(curr, candidates[idx])
+	backtrack(idx, currTotal+candidates[idx], target, curr, candidates, result)
+
+	// backtrack then go to the next index
 	curr = curr[:len(curr)-1]
-	backtrack1(i+1, total, target, candidates, curr, result)
+	backtrack(idx+1, currTotal, target, curr, candidates, result)
 }
