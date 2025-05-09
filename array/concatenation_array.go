1c18874a7fb2d638b2c28a264d865d8541245a1f
izzan zahrial <izzanzahrial@outlook.com>
2025-05-09 10:22:48 +0700

    add need code concatenation array

---
+++ array/concatenation_array.go
@@ -0,0 +1,14 @@
+package array
+
+// https://neetcode.io/problems/concatenation-of-array
+func GetConcatenationArray(nums []int) []int {
+	return append(nums, nums...)
+}
+
+func GetConcatenationArray2(nums []int) []int {
+	for _, num := range nums {
+		nums = append(nums, num)
+	}
+
+	return nums
+}
