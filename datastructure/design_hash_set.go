ab53d9aed85ee66c414c2f014de18e4475609c29
izzan zahrial <izzanzahrial@outlook.com>
2025-05-09 11:06:02 +0700

    add design hash set

---
+++ datastructure/design_hash_set.go
@@ -0,0 +1,33 @@
+package datastructure
+
+// https://leetcode.com/problems/design-hashset/
+type MyHashSet struct {
+	hash map[int]struct{}
+}
+
+func Constructor() MyHashSet {
+	return MyHashSet{
+		hash: make(map[int]struct{}),
+	}
+}
+
+func (this *MyHashSet) Add(key int) {
+	this.hash[key] = struct{}{}
+}
+
+func (this *MyHashSet) Remove(key int) {
+	delete(this.hash, key)
+}
+
+func (this *MyHashSet) Contains(key int) bool {
+	_, contains := this.hash[key]
+	return contains
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
