f009dd2808091b81c7e15546a3de851aec24257f
izzan zahrial <izzanzahrial@outlook.com>
2025-05-14 06:02:22 +0700

    add evaluate reverse polish nation

--- stacknqueue/evaluateRPN.go
+++ stacknqueue/evaluateRPN.go
@@ -2,6 +2,7 @@ package stacknqueue
 
 import "strconv"
 
+// https://leetcode.com/problems/evaluate-reverse-polish-notation/
 func evalRPN(tokens []string) int {
 	var result int
 	var stack []string
@@ -41,4 +42,38 @@ func evalRPN(tokens []string) int {
 	result, _ = strconv.Atoi(stack[0])
 
 	return result
+}
+
+func evalRPN2(tokens []string) int {
+	var stack []int
+
+	for _, char := range tokens {
+		switch char {
+		case "+":
+			num1 := stack[len(stack)-2]
+			num2 := stack[len(stack)-1]
+			addResult := num1 + num2
+			stack = append(stack[:len(stack)-2], addResult)
+		case "-":
+			num1 := stack[len(stack)-2]
+			num2 := stack[len(stack)-1]
+			subResult := num1 - num2
+			stack = append(stack[:len(stack)-2], subResult)
+		case "*":
+			num1 := stack[len(stack)-2]
+			num2 := stack[len(stack)-1]
+			timesResult := num1 * num2
+			stack = append(stack[:len(stack)-2], timesResult)
+		case "/":
+			num1 := stack[len(stack)-2]
+			num2 := stack[len(stack)-1]
+			divResult := num1 / num2
+			stack = append(stack[:len(stack)-2], divResult)
+		default:
+			num, _ := strconv.Atoi(char)
+			stack = append(stack, num)
+		}
+	}
+
+	return stack[0]
 }
