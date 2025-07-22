eb546c769015d1a9679d42035bcbae8d02afa3c8
izzan zahrial <izzanzahrial@outlook.com>
2025-07-22 20:55:43 +0700

    add longest consecutive

--- hash/longest_consecutive_sequence.go
+++ hash/longest_consecutive_sequence.go
@@ -1,6 +1,7 @@
 package hash
 
 // https://leetcode.com/problems/longest-consecutive-sequence/
+// search descending (backward from the biggest to the smallest number)
 func longestConsecutive(nums []int) int {
 	numMap := make(map[int]bool)
 
@@ -25,4 +26,39 @@ func longestConsecutive(nums []int) int {
 	}
 
 	return result
+}
+
+// search ascending (forward from smallest number to the biggest number)
+func longestConsecutive2(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	numsMap := make(map[int]bool)
+	for _, num := range nums {
+		numsMap[num] = true
+	}
+
+	var longest int
+	for num := range numsMap {
+		// search for the start of the number, if the current number - 1 exists
+		// meaning current number is not the start of the number
+		if numsMap[num-1] {
+			continue
+		}
+
+		currNum := num
+		currLength := 1
+
+		for numsMap[currNum+1] {
+			currNum++
+			currLength++
+		}
+
+		if currLength > longest {
+			longest = currLength
+		}
+	}
+
+	return longest
 }
