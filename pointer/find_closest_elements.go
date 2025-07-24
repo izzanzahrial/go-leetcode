c8a19f556029a74d4220d09d1e7504c65ab659bc
izzan zahrial <izzanzahrial@outlook.com>
2025-07-24 20:21:55 +0700

    add find k closest elements

---
+++ pointer/find_closest_elements.go
@@ -0,0 +1,23 @@
+package pointer
+
+// https://leetcode.com/problems/find-k-closest-elements/
+func findClosestElements(arr []int, k int, x int) []int {
+	for len(arr) > k {
+		absA := abs(arr[0] - x)
+		absB := abs(arr[len(arr)-1] - x)
+		if absA > absB {
+			arr = arr[1:]
+		} else {
+			arr = arr[:len(arr)-1]
+		}
+	}
+
+	return arr
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
